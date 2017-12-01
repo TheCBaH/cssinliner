e8735f0018aeb04aaa8b5afe530221330c0490c0
Vladimir N. Silyaev <TheCBaH@gmail.com>
2017-12-01 00:27:04 -0800

    Reformat

--- cssinliner.ml
+++ cssinliner.ml
@@ -5,169 +5,181 @@
 #require "str";;
 *)
 
-
 let css_parser =
   let open Angstrom in
-  let wspace = skip_while (function
-                     '\x20' | '\x0a' | '\x0d' | '\x09' -> true
-                     | _ -> false) in
-  let raw_block = char '{' *> wspace *> lift2 (fun s _ -> s)
-                (take_while (function
-                    '}' -> false
-                    | _ -> true))  (char '}') in
-  let id = take_while (function
-                 'A'..'Z' | 'a'..'z' | '0'..'0' | '_' | '-' -> true
-                 | _ -> false) in
+  let wspace =
+    skip_while (function ' ' | '\n' | '\r' | '\t' -> true | _ -> false)
+  in
+  let raw_block =
+    char '{' *> wspace
+    *> lift2
+         (fun s _ -> s)
+         (take_while (function '}' -> false | _ -> true))
+         (char '}')
+  in
+  let id =
+    take_while (function
+      | 'A'..'Z' | 'a'..'z' | '0'..'0' | '_' | '-' -> true
+      | _ -> false )
+  in
   let semicolon_line =
-    wspace *> take_while (function ';'|'{'|'}' -> false | _ -> true) <* (char ';') <* wspace in
-  let block = char '{' *> (many semicolon_line) <* char '}' in
-  let re_trailwspace = Str.regexp "[\x20\x0d\x0a\x09]+$" in
+    wspace *> take_while (function ';' | '{' | '}' -> false | _ -> true)
+    <* char ';' <* wspace
+  in
+  let block = char '{' *> many semicolon_line <* char '}' in
+  let re_trailwspace = Str.regexp "[ \r\n\t]+$" in
   let skip_trailwspace str =
-    try
-      Str.search_forward re_trailwspace str 0 |> String.sub str 0
-    with Not_found -> str in
-  let at_rule = wspace *> char '@' *>
-                  lift3 (fun id () block -> `At (id,block))
-                    id  wspace (
-                      (lift (fun b -> `Block (skip_trailwspace b)) raw_block) <|>
-                        (lift (fun l -> `Line l) semicolon_line)) in
-  let selector = wspace *> take_while (function
-                       '{'|';'|'}' -> false
-                     | _ -> true
-                             ) in
-  let re_colummn = Str.regexp ":[\x20\x0d\x0a\x09]*" in
+    try Str.search_forward re_trailwspace str 0 |> String.sub str 0
+    with Not_found -> str
+  in
+  let at_rule =
+    wspace *> char '@'
+    *> lift3
+         (fun id () block -> `At (id, block))
+         id wspace
+         ( lift (fun b -> `Block (skip_trailwspace b)) raw_block
+         <|> lift (fun l -> `Line l) semicolon_line )
+  in
+  let selector =
+    wspace *> take_while (function '{' | ';' | '}' -> false | _ -> true)
+  in
+  let re_colummn = Str.regexp ":[ \r\n\t]*" in
   let split_style str =
     (* Str.bounded_split re_colummn str 2 *)
     match Str.bounded_split re_colummn str 2 with
-      [n;v] -> n,v
+    | [n; v] -> (n, v)
     | _ -> failwith "style"
   in
-  let rule = lift2 (fun selector block ->
-                 `Rule ( (skip_trailwspace selector),List.map split_style block)) selector block in
+  let rule =
+    lift2
+      (fun selector block ->
+        `Rule (skip_trailwspace selector, List.map split_style block))
+      selector block
+  in
   many (at_rule <|> rule)
 
-type state = {
-    load: (string list * string) -> string option;
-    clean_class: bool;
-}
+
+type state = {load: string list * string -> string option; clean_class: bool}
 
 let parse_one_style style str =
   match str with
-    None -> style
+  | None -> style
   | Some str ->
-     match Angstrom.parse_only css_parser (`String str) with
-       Result.Ok l ->
-        List.append l style
-     | _ -> style
+    match Angstrom.parse_only css_parser (`String str) with
+    | Result.Ok l -> List.append l style
+    | _ -> style
 
-let select_internal_styles html =
-  let open Soup in
-  html $$ "style"
+
+let select_internal_styles html = Soup.(html $$ "style")
 
 let load_internal_style html styles =
   let open Soup in
-  select_internal_styles html |>
-    fold (fun s n ->
-        leaf_text n |> parse_one_style s
-      ) styles
+  select_internal_styles html
+  |> fold (fun s n -> leaf_text n |> parse_one_style s) styles
 
-let select_external_styles html =
-  let open Soup in
-  html $$ "link[rel=stylesheet]"
+
+let select_external_styles html = Soup.(html $$ "link[rel=stylesheet]")
 
 let get_external_styles html =
   let open Soup in
-  select_external_styles html |>
-    fold (fun l n ->
-        match attribute "href" n  with
-          None -> l
-        | Some s -> ([],s)::l) [] |> List.rev
+  select_external_styles html
+  |> fold
+       (fun l n ->
+         match attribute "href" n with None -> l | Some s -> ([], s) :: l)
+       []
+  |> List.rev
+
 
 let load_external_style state links style =
-  List.fold_left (fun s l ->
-      state.load l |> parse_one_style s) style links
+  List.fold_left (fun s l -> state.load l |> parse_one_style s) style links
+
 
 let add_style styles node =
   let open Soup in
-  let subst = match name node with
-    "table" -> [
-        "float","align";
-        "background-color","bgcolor";
-        "width","width";
-        "height","height"
-      ]
-    | "tr" -> [
-        "background-color", "bgcolor";
-        "vertical-align", "valign";
-        "text-align", "align"
-      ]
-    | "td" | "th" -> [
-            "background-color", "bgcolor";
-            "width", "width";
-            "height", "height";
-            "vertical-align", "valign";
-            "text-align", "align";
-            "white-space", "nowrap";
-      ]
-    |"tbody"|"thead"|"tfoot" -> [
-            "vertical-align", "valign";
-            "text-align", "align";
-        ]
-    | _ -> [] in
+  let subst =
+    match name node with
+    | "table" ->
+        [ ("float", "align")
+        ; ("background-color", "bgcolor")
+        ; ("width", "width")
+        ; ("height", "height") ]
+    | "tr" ->
+        [ ("background-color", "bgcolor")
+        ; ("vertical-align", "valign")
+        ; ("text-align", "align") ]
+    | "td" | "th" ->
+        [ ("background-color", "bgcolor")
+        ; ("width", "width")
+        ; ("height", "height")
+        ; ("vertical-align", "valign")
+        ; ("text-align", "align")
+        ; ("white-space", "nowrap") ]
+    | "tbody" | "thead" | "tfoot" ->
+        [("vertical-align", "valign"); ("text-align", "align")]
+    | _ -> []
+  in
   let styles =
     match subst with
-      [] -> styles
+    | [] -> styles
     | subst ->
-       List.filter (fun (k,v) ->
-           match List.assoc_opt k subst with
-             Some k' ->  set_attribute k' v node;false
-           | None -> true
-         ) styles in
-
+        List.filter
+          (fun (k, v) ->
+            match List.assoc_opt k subst with
+            | Some k' -> set_attribute k' v node ; false
+            | None -> true)
+          styles
+  in
   match styles with
-    [] -> ()
+  | [] -> ()
   | styles ->
-     let style = (styles
-                  |> List.map (fun (k,v) -> k ^ ":" ^ v)
-                  |> String.concat ";")  ^ ";" in
-     let style =
-       match attribute "style" node with
-         Some s -> style ^ s
-       | None -> style in
-     set_attribute "style" style node
+      let style =
+        (styles |> List.map (fun (k, v) -> k ^ ":" ^ v) |> String.concat ";")
+        ^ ";"
+      in
+      let style =
+        match attribute "style" node with Some s -> style ^ s | None -> style
+      in
+      set_attribute "style" style node
 
-let re_comawspace = Str.regexp "[\x20\x0d\x0a\x09]*,[\x20\x0d\x0a\x09]*"
+
+let re_comawspace = Str.regexp "[ \r\n\t]*,[ \r\n\t]*"
+
 let re_pseudos =
-    ".*\\(" ^ (["hover"; "active"; "focus"; "visited"; "link"] |> List.map (fun s -> ":" ^ s ) |> String.concat "\\|"  ) ^ "\\).*" |> Str.regexp
+  ".*\\("
+  ^ ( ["hover"; "active"; "focus"; "visited"; "link"]
+    |> List.map (fun s -> ":" ^ s) |> String.concat "\\|" )
+  ^ "\\).*"
+  |> Str.regexp
+
 
 let apply_style style html =
   let open Soup in
-  List.iter (function
-        `Rule (selector,styles) ->
-         Str.split re_comawspace selector
-         |> List.iter (fun selector ->
-            if not (Str.string_match re_pseudos selector 0) then
-                try
-                 html $$ selector |> iter (add_style styles)
-                with _ -> "Can't handle selector:'" ^ selector ^ "'" |> failwith
-                 )
-      | _ -> ()) style;
+  List.iter
+    (function
+        | `Rule (selector, styles) ->
+            Str.split re_comawspace selector
+            |> List.iter (fun selector ->
+                   if not (Str.string_match re_pseudos selector 0) then
+                     try html $$ selector |> iter (add_style styles)
+                     with _ ->
+                       "Can't handle selector:'" ^ selector ^ "'" |> failwith )
+        | _ -> ())
+    style ;
   html
+
 
 let clean state html =
   let open Soup in
-  select_internal_styles html |> iter delete;
-  select_external_styles html |> iter delete;
-  if state.clean_class then
-    html $$ "*" |> iter (fun n ->
-                       delete_attribute "class" n;
-                       delete_attribute "id" n;
-                     );
+  select_internal_styles html |> iter delete ;
+  select_external_styles html |> iter delete ;
+  if state.clean_class then html $$ "*"
+    |> iter (fun n -> delete_attribute "class" n ; delete_attribute "id" n) ;
   html
 
-let inline_css ?(clean_class=true) ?(load=(fun _ -> None)) html =
+
+let inline_css ?(clean_class= true) ?(load= fun _ -> None) html =
   let open Soup in
-  let state = {load;clean_class} in
+  let state = {load; clean_class} in
   let html = parse html in
   let ext_styles = get_external_styles html in
   let style = [] in
@@ -175,3 +187,4 @@ let inline_css ?(clean_class=true) ?(load=(fun _ -> None)) html =
   let style = load_internal_style html style in
   (* style *)
   html |> apply_style style |> clean state |> pretty_print
+
--- test/test.ml
+++ test/test.ml
@@ -1,62 +1,65 @@
 open Cssinliner
 
-let tests = [
-    "alpha.html";
-    "cascading.html";
-    "character-entities.html";
-    "css-quotes.html";
-    "direct-descendents.html";
-    "direct-descendents.html";
-    "font-quotes.html";
-    "table-attr.html";
-    "id.html";
-    "identical-important.html";
-    "ignore-pseudos.html";
-    "important.html";
-    "in.html";
-    "malformed.html";
-    "media.html";
-    "no_css.html";
-    (* "normalize.html";  comments in CSS *)
-    "preserve-events.html";
-    "regression-media.html";
-    "regression-selector-newline.html";
-    (* "remote_url.html"; remote *)
-    "remove-html-selectors.html";
-    "spaces_in_path.html";
-    "specificity.html";
-    "style-preservation.html";
-    "tag.html";
-    "two_styles.html";
-    "width-attr.html";
-    "xhtml.html";
-    (* "yui-reset.html"; comments in CSS *)
-  ]
+let tests =
+  [ (* "yui-reset.html"; comments in CSS *)
+    "alpha.html"
+  ; "cascading.html"
+  ; "character-entities.html"
+  ; "css-quotes.html"
+  ; "direct-descendents.html"
+  ; "direct-descendents.html"
+  ; "font-quotes.html"
+  ; "table-attr.html"
+  ; "id.html"
+  ; "identical-important.html"
+  ; "ignore-pseudos.html"
+  ; "important.html"
+  ; "in.html"
+  ; "malformed.html"
+  ; "media.html"
+  ; "no_css.html"
+  ; (* "normalize.html";  comments in CSS *)
+    "preserve-events.html"
+  ; "regression-media.html"
+  ; "regression-selector-newline.html"
+  ; (* "remote_url.html"; remote *)
+    "remove-html-selectors.html"
+  ; "spaces_in_path.html"
+  ; "specificity.html"
+  ; "style-preservation.html"
+  ; "tag.html"
+  ; "two_styles.html"
+  ; "width-attr.html"
+  ; "xhtml.html" ]
 
-let tests_no_clean = [
- "class+id.html";
- "class.html"
-  ]
+
+let tests_no_clean = ["class+id.html"; "class.html"]
 
 let root = "test/"
+
 let root = Sys.argv.(1) ^ root
+
 let src_dir = root ^ "inline-css/test/fixtures/"
+
 let dst_dir = root ^ "results/"
 
-let write_result file str = 
-    let ch_out = dst_dir ^ file |> open_out  in
-    output_string ch_out str;
-    close_out ch_out
+let write_result file str =
+  let ch_out = dst_dir ^ file |> open_out in
+  output_string ch_out str ; close_out ch_out
+
 
 let test_one ~clean_class file =
-    let open Soup in
-    print_endline file;
-    let load (_,file) = Some (read_file (src_dir ^ file)) in
-    src_dir ^ file |> read_file |> inline_css ~clean_class ~load |> write_result file
+  let open Soup in
+  print_endline file ;
+  let load (_, file) = Some (read_file (src_dir ^ file)) in
+  src_dir ^ file |> read_file |> inline_css ~clean_class ~load
+  |> write_result file
+
 
 let _ =
-    List.iter (test_one ~clean_class:true) tests;
-    List.iter (test_one ~clean_class:false) tests_no_clean
+  List.iter (test_one ~clean_class:true) tests ;
+  List.iter (test_one ~clean_class:false) tests_no_clean
+
 
 (*
 let _ = test_one ~clean_class:false "class+id.html"
